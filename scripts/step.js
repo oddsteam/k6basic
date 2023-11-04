0117113a40de9757cb3b5fdc58edbd7e4f4b3346
Chaya Phuwichit <chaya@odds.team>
2023-11-04 13:57:03 +0700

    add option

--- scripts/step.js
+++ scripts/step.js
@@ -1,9 +1,22 @@
 import http from "k6/http";
 import { check, group, sleep } from "k6";
 
+export const options = {
+    stages: [
+      { duration: '30s', target: 20 },
+      { duration: '1m30s', target: 10 },
+      { duration: '20s', target: 0 },
+    ],
+  };
+
 export default async function () {
   let token = "";
-
+  group("GET Hello", function () {
+    let res = http.get("http://host.docker.internal:3001/hello");
+    check(res, {
+      "is status 200": (r) => r.status === 200,
+    });
+  });
   group("GET Token", function () {
     let res = http.get("http://host.docker.internal:3001/token");
     //console.log(res.body);
